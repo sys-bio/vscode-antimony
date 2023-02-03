310d9a0c8f00eebb342d710a972d7f2723c4f716
Eva Liu <evaxinerliu@gmail.com>
2023-02-03 12:39:23 -0800

    linux implementation to linux home dir

--- vscode-antimony/src/server/virtualEnvLinux.sh
+++ vscode-antimony/src/server/virtualEnvLinux.sh
@@ -18,17 +18,32 @@ ve() {
     local py=${1:-python3}
     local venv="venv_vscode_antimony_virtual_env"
 
-    local bin="${venv}/bin/activate"
-
-    echo "running install virtual env"
-    # If not already in virtualenv
-    # $VIRTUAL_ENV is being set from $venv/bin/activate script
     echo "Creating and activating virtual environment ${venv}"
     python3 -m venv ${venv} --system-site-packages
-    echo "export PYTHON=${py}" >> ${bin}    # overwrite ${python} on .zshenv
+    echo "appdirs==1.4.4
+    certifi==2020.12.5
+    chardet==4.0.0
+    colorama==0.4.4
+    colorlog==4.8.0
+    easydev==0.11.0
+    idna==2.10
+    git+https://github.com/lark-parser/lark.git@5b8c04ca83b9#egg=lark_parser
+    libChEBIpy==1.0.10
+    pexpect==4.8.0
+    ptyprocess==0.7.0
+    pygls==0.9.1
+    requests==2.25.1
+    requests-cache==0.5.2
+    six==1.15.0
+    urllib3==1.26.4
+    antimony
+    bioservices==1.8.3
+    # ols_client==0.0.9
+    AMAS-sb==0.0.1
+    orjson==3.8.0" > "$HOME/$venv/all-requirements.txt"
     echo "Upgrading pip"
     pip install --upgrade pip
-    python3 -m pip --disable-pip-version-check install -t ./pythonFiles/lib/python --no-cache-dir --upgrade -r all-requirements.txt && success=1
+    python3 -m pip --disable-pip-version-check install -t ./pythonFiles/lib/python --no-cache-dir --upgrade -r "$HOME/$venv/all-requirements.txt" && success=1
 }
 
 ve "$@"
