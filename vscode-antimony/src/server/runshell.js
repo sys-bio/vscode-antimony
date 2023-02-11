4e89022be0d8d878450b09a76aa7ddf0dfb93a69
Anish Konanki <anishkonanki@gmail.com>
2023-02-10 19:16:20 -0800

    updated path

--- vscode-antimony/src/server/runshell.js
+++ vscode-antimony/src/server/runshell.js
@@ -11,11 +11,8 @@ const current_path_to_dir = path.join(__dirname, '..');
 if (os.platform().toString() === 'darwin') {
     shell.exec(`sh ${current_path_to_silicon_shell}`)
 } else if (os.platform().toString() === 'win32') {
-    const path_to_win_shell = path.join(__dirname, 'server');
-
-    shell.cd(path_to_win_shell)
+    const path_to_win_shell = path.join(__dirname);
     shell.exec(`${path_to_win_shell}\\virtualEnvWin.bat`)
-        
 } else if (os.platform().toString() === 'linux') {
     shell.cd(current_path_to_dir)
     shell.exec(`sh ${current_path_to_linux_shell}`);
