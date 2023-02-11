ab17528d21c57a9a6ebd5ac4a6e24d5671cd1d93
Eva Liu <evaxinerliu@gmail.com>
2023-02-10 19:10:58 -0800

    changes to run shell file

---
+++ vscode-antimony/src/server/runshell.js
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+const os = require('os');
+const path = require('path');
+
+const shell = require('shelljs');
+const current_path_to_silicon_shell = path.join(__dirname, '..', 'server', 'virtualEnvSilicon.sh');
+const current_path_to_linux_shell = path.join(__dirname, '..', 'server', 'virtualEnvLinux.sh');
+const current_path_to_dir = path.join(__dirname, '..');
+
+if (os.platform().toString() === 'darwin') {
+    shell.exec(`sh ${current_path_to_silicon_shell}`)
+} else if (os.platform().toString() === 'win32') {
+    const path_to_win_shell = path.join(__dirname, 'server');
+
+    shell.cd(path_to_win_shell)
+    shell.exec(`${path_to_win_shell}\\virtualEnvWin.bat`)
+        
+} else if (os.platform().toString() === 'linux') {
+    shell.cd(current_path_to_dir)
+    shell.exec(`sh ${current_path_to_linux_shell}`);
+}
