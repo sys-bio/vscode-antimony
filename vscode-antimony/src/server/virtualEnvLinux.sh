394f821ca63407cc055b5ca2c6c390ca4a473c1c
Eva Liu <evaxinerliu@gmail.com>
2023-02-10 19:31:19 -0800

    changes to run shell file

--- vscode-antimony/src/server/virtualEnvLinux.sh
+++ vscode-antimony/src/server/virtualEnvLinux.sh
@@ -22,7 +22,7 @@ ve() {
     python3 -m venv $HOME/${venv}
     echo "Upgrading pip"
     pip install --upgrade pip
-    echo $'appdirs==1.4.4\ncertifi==2020.12.5\nchardet==4.0.0\ncolorama==0.4.4\ncolorlog==4.8.0 \neasydev==0.11.0\nidna==2.10\ngit+https://github.com/lark-parser/lark.git@5b8c04ca83b9#egg=lark_parser\nlibChEBIpy==1.0.10\npexpect==4.8.0\nptyprocess==0.7.0\npygls==0.9.1\nrequests==2.25.1\nrequests-cache==0.5.2\nsix==1.15.0\nurllib3==1.26.4\nantimony\nbioservices==1.8.3\n# ols_client==0.0.9\nAMAS-sb==0.0.1\norjson==3.8.0 ' > $HOME/${venv}/all-requirements.txt
+    echo 'appdirs==1.4.4\ncertifi==2020.12.5\nchardet==4.0.0\ncolorama==0.4.4\ncolorlog==4.8.0 \neasydev==0.11.0\nidna==2.10\ngit+https://github.com/lark-parser/lark.git@5b8c04ca83b9#egg=lark_parser\nlibChEBIpy==1.0.10\npexpect==4.8.0\nptyprocess==0.7.0\npygls==0.9.1\nrequests==2.25.1\nrequests-cache==0.5.2\nsix==1.15.0\nurllib3==1.26.4\nantimony\nbioservices==1.8.3\n# ols_client==0.0.9\nAMAS-sb==0.0.1\norjson==3.8.0 ' > $HOME/${venv}/all-requirements.txt
     python3 -m pip --disable-pip-version-check install -t $HOME/${venv}/lib/python3.10/site-packages --no-cache-dir --upgrade -r $HOME/${venv}/all-requirements.txt && success=1
 }
 
