0b75c4acbb446946bec210794a422e5abb2b82ed
Eva Liu <evaxinerliu@gmail.com>
2023-02-17 16:24:15 -0800

    fixed shelljs module?

--- vscode-antimony/src/server/runshell.js
+++ vscode-antimony/src/server/runshell.js
@@ -1,19 +1,21 @@
 #!/bin/bash
-
+// import * as path from 'path';
+// import * as os from 'os';
 const os = require('os');
 const path = require('path');
-
-const shell = require('shelljs');
+const { exec } = require("child_process")
+// import shell from 'shelljs';
+// const shell = require('shelljs');
 const current_path_to_silicon_shell = path.join(__dirname, '..', 'server', 'virtualEnvSilicon.sh');
 const current_path_to_linux_shell = path.join(__dirname, '..', 'server', 'virtualEnvLinux.sh');
 const current_path_to_dir = path.join(__dirname, '..');
 
 if (os.platform().toString() === 'darwin') {
-    shell.exec(`sh ${current_path_to_silicon_shell}`)
+    exec(`bash ${current_path_to_silicon_shell}`)
 } else if (os.platform().toString() === 'win32') {
     const path_to_win_shell = path.join(__dirname);
-    shell.exec(`${path_to_win_shell}\\virtualEnvWin.bat`)
+    exec(`${path_to_win_shell}\\virtualEnvWin.bat`)
 } else if (os.platform().toString() === 'linux') {
     shell.cd(current_path_to_dir)
-    shell.exec(`sh ${current_path_to_linux_shell}`);
+    exec(`bash ${current_path_to_linux_shell}`);
 }
