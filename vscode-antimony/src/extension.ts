356795f89b7995804a58cc22fa2831df07040ebd
evaliu2002 <evaxinerliu@gmail.com>
2023-03-20 22:16:30 -0700

    change node path

--- vscode-antimony/src/extension.ts
+++ vscode-antimony/src/extension.ts
@@ -747,33 +747,22 @@ async function installEnv() {
 		}
 	} else {
 		if (platform === "win32" || platform === "win64") {
-			const installDeno = async (): Promise<void> => {
-				// Download Deno
-				const downloadUrl = 'https://github.com/denoland/deno/releases/latest/download/deno-x86_64-pc-windows-msvc.zip';
-				await execPowerShell(`Invoke-WebRequest -Uri ${downloadUrl} -OutFile "$env:TEMP\\deno.zip"`);
-			  
-				// Extract Deno
-				const outputFolder = `${process.env.ProgramFiles}\\Deno`;
-				await execPowerShell(`New-Item -ItemType Directory -Force -Path ${outputFolder}`);
-				await execPowerShell(`Expand-Archive -LiteralPath "$env:TEMP\\deno.zip" -DestinationPath ${outputFolder}`);
-			  
-				// Add Deno to the PATH environment variable
-				const existingPath = process.env.Path;
-				const newPath = `${existingPath};${outputFolder}\\bin`;
-				await execPowerShell(`[Environment]::SetEnvironmentVariable("Path", "${newPath}", "Machine")`);
-			  };
-			  
-			  const execPowerShell = async (command: string): Promise<void> => {
-				await exec(`powershell -Command "${command}"`, { windowsHide: true });
-			  };
-			  
-			  // Run the installation script
-			await installDeno().then(() => console.log('Deno has been installed successfully.'));
-			progressBar('deno run ' + current_path_to_jsscript);
-		} else {
-			console.log('Virtual environment is not activated');
-			progressBar('node ' + current_path_to_jsscript);
+			exec("where node", (error, stdout, stderr) => {
+				if (error) {
+				  console.error(`exec error: ${error}`);
+				  return;
+				}
+				const nodePath = stdout.trim();
+				if (nodePath === '') {
+					const nodePath = "C:\\Program Files\\nodejs";
+					console.log(`Node.js path: ${nodePath}`);
+				}
+				process.env.PATH += `;${nodePath}`;
+				console.log(`Node.js path: ${nodePath}`);
+			 });
 		}
+		console.log('Virtual environment is not activated');
+		progressBar('node ' + current_path_to_jsscript);
 	}
 }
 
