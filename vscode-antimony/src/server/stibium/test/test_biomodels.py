f9905c9ca318644a5ce6bb5b85d9aba86334d34e
evaliu2002 <evaxinerliu@gmail.com>
2023-04-01 23:49:01 -0700

    biomodel testing

---
+++ vscode-antimony/src/server/stibium/test/test_biomodels.py
@@ -0,0 +1,24 @@
+'Tests concerning checking for errors in models from biomodels feature.'
+
+import json
+from logging import debug
+from stibium import api
+from main import get_model
+import os
+from pygls.workspace import Document
+
+
+# TODO add more tests as more syntax features are added
+def test_all_biomodels():
+    f = open('biomodels_list.json')
+    models = json.load(f)
+    for model in enumerate(models['models']):
+        debug.log(model)
+        get_model(model)
+        f = os.path.join(model + '.ant')
+        doc = Document(os.path.abspath(f))
+        ant_file = api.AntFile(doc.path, doc.source)
+        l_issues = ant_file.get_issues()
+        error_count = 0
+        for issue in l_issues:
+            assert(False)
