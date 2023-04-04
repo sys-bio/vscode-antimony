50a31ecb19e45ac1d4e5ac04020a77749e8a1491
shshdhdhdhdyx <anishkonanki@gmail.com>
2023-04-04 14:21:46 -0700

    testing progress

--- vscode-antimony/src/server/stibium/test/test_biomodels.py
+++ vscode-antimony/src/server/stibium/test/test_biomodels.py
@@ -3,22 +3,34 @@
 import json
 from logging import debug
 from stibium import api
-from main import get_model
 import os
 from pygls.workspace import Document
 import pytest
+import io
+import requests
+import zipfile
 
 f = open('biomodels_list.json')
 models = json.load(f)
 
+def get_model(model: str):
+    model_download_url = ("https://www.ebi.ac.uk/biomodels/search/download?models={model}").format(
+        model=model
+    )
+    response = requests.get(model_download_url, stream=True)
+    extract = zipfile.ZipFile(io.BytesIO(response.content))
+    data = io.TextIOWrapper(extract.open(extract.namelist()[0]), encoding="utf-8", newline="").read()
+    extract.close()
+    return extract.namelist()[0], data
 
 # TODO add more tests as more syntax features are added
 @pytest.mark.parametrize('models', [models])
 def test_all_biomodels():
     for model in models:
         debug.log(model)
-        get_model(model)
-        f = os.path.join(model + '.ant')
+        filename, cur_model = get_model(model)
+        # store the data in a temp file or get the extracted SBML file and convert it to Antimony
+        f = os.path.join(filename + '.ant')
         doc = Document(os.path.abspath(f))
         ant_file = api.AntFile(doc.path, doc.source)
         l_issues = ant_file.get_issues()
