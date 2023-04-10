48dd844a67f4dc126deefeaed527b58a4b9a1710
evaliu2002 <evaxinerliu@gmail.com>
2023-04-10 08:13:53 -0700

    test runs, but errors

--- vscode-antimony/src/server/stibium/test/test_biomodels.py
+++ vscode-antimony/src/server/stibium/test/test_biomodels.py
@@ -2,36 +2,22 @@
 
 import json
 from logging import debug
+
+import pytest
 from stibium import api
 import os
 from pygls.workspace import Document
-import pytest
-import io
-import requests
-import zipfile
+from os import listdir
 
-f = open('biomodels_list.json')
-models = json.load(f)
-
-def get_model(model: str):
-    model_download_url = ("https://www.ebi.ac.uk/biomodels/search/download?models={model}").format(
-        model=model
-    )
-    response = requests.get(model_download_url, stream=True)
-    extract = zipfile.ZipFile(io.BytesIO(response.content))
-    data = io.TextIOWrapper(extract.open(extract.namelist()[0]), encoding="utf-8", newline="").read()
-    extract.close()
-    return extract.namelist()[0], data
+path="/Users/evaliu/Documents/vscode-antimony/vscode-antimony/src/server/stibium/test/biomodels"
+bio_list = os.listdir(path)
 
 # TODO add more tests as more syntax features are added
-@pytest.mark.parametrize('models', [models])
-def test_all_biomodels():
-    for model in models:
-        debug.log(model)
-        filename, cur_model = get_model(model)
-        # store the data in a temp file or get the extracted SBML file and convert it to Antimony
-        f = os.path.join(filename + '.ant')
-        doc = Document(os.path.abspath(f))
-        ant_file = api.AntFile(doc.path, doc.source)
-        l_issues = ant_file.get_issues()
-        assert l_issues == []
+@pytest.mark.parametrize('models', bio_list)
+def test_all_biomodels(models):
+    # store the data in a temp file or get the extracted SBML file and convert it to Antimony
+    f = os.path.join(path + "/" + models)
+    doc = Document(os.path.abspath(f))
+    ant_file = api.AntFile(doc.path, doc.source)
+    l_issues = ant_file.get_issues()
+    assert l_issues == []
