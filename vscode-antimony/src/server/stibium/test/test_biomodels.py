e5ddadcaeef9e68035f633b76e90e50bae88a94d
Eva Liu <evaliu02@uw.edu>
2023-04-13 20:25:32 -0700

    testing temp file xml to ant

--- vscode-antimony/src/server/stibium/test/test_biomodels.py
+++ vscode-antimony/src/server/stibium/test/test_biomodels.py
@@ -8,17 +8,50 @@ from stibium import api
 import os
 from pygls.workspace import Document
 from os import listdir
+import antimony
+import tempfile
 
 # Change path later
-path="/Users/evaliu/Documents/vscode-antimony/vscode-antimony/src/server/stibium/test/biomodels"
+path="/Users/evaliu02/Documents/vscode-antimony/vscode-antimony/src/server/stibium/test/biomodels"
 bio_list = os.listdir(path)
+
+def _get_antimony_str(sbml):
+    if sbml is None:
+        return {
+            'error': 'Cannot open file'
+        }
+    antimony.clearPreviousLoads()
+    antimony.freeAll()
+    try:
+        isfile = os.path.isfile(sbml)
+    except ValueError:
+        return {
+            'error': 'Cannot open file'
+        }
+    if isfile:
+        ant = antimony.loadSBMLFile(sbml)
+        if ant < 0:
+            return {
+                'error': 'Antimony -  {}'.format(antimony.getLastError())
+            }
+        ant_str = antimony.getAntimonyString(None)
+        return {
+            'ant_str': ant_str
+        }
+    else:
+        return {
+            'error': 'Not a valid file'
+        }
 
 # TODO add more tests as more syntax features are added
 @pytest.mark.parametrize('models', bio_list)
 def test_all_biomodels(models):
     # store the data in a temp file or get the extracted SBML file and convert it to Antimony
     f = os.path.join(path + "/" + models)
-    doc = Document(os.path.abspath(f))
-    ant_file = api.AntFile(doc.path, doc.source)
-    l_issues = ant_file.get_issues()
+    ant_str = _get_antimony_str(os.path.abspath(f))
+    temp_ant_file = tempfile.TemporaryFile(mode='w+t', delete=True)
+    temp_ant_file.write(ant_str.get('ant_str'))
+    # xml_file = api.AntFile(doc.path, doc.source)
+    l_issues = temp_ant_file.get_issues()
+    temp_ant_file.close()
     assert l_issues == []
