52a7a60960d6b3e37d2c03e0b93cd74438f6e881
evaliu2002 <evaxinerliu@gmail.com>
2023-05-14 22:25:40 -0700

    mac install fix

--- vscode-antimony/src/server/virtualEnvSilicon.sh
+++ vscode-antimony/src/server/virtualEnvSilicon.sh
@@ -22,7 +22,7 @@ ve() {
 
     echo "Creating and activating virtual environment ${venv}"
     # virtualenv ~/[${venv}]
-    sudo -u $USER $HOME/Downloads/Vscode_Antimony_Setup/bin/python3.9 -m venv $HOME/${venv} --system-site-package
+    sudo -u $USER /Users/Vscode_Antimony_Setup/Vscode_Antimony_Setup/bin/python3.9 -m venv $HOME/${venv} --system-site-package
     echo "Upgrading pip"
     /Users/Vscode_Antimony_Setup/Vscode_Antimony_Setup/bin/python3.9 -m pip install --upgrade pip
     echo $'appdirs==1.4.4\ncertifi==2020.12.5\nchardet==4.0.0\ncolorama==0.4.4\ncolorlog==4.8.0 \neasydev==0.11.0\nidna==2.10\ngit+https://github.com/lark-parser/lark.git@5b8c04ca83b9#egg=lark_parser\nlibChEBIpy==1.0.10\npexpect==4.8.0\nptyprocess==0.7.0\npygls==0.9.1\nrequests==2.25.1\nrequests-cache==0.5.2\nsix==1.15.0\nurllib3==1.26.4\nantimony\nbioservices==1.8.3\n# ols_client==0.0.9\nAMAS-sb==0.0.1\norjson==3.8.0\nnumpy==1.24.2' > $HOME/${venv}/all-requirements.txt
