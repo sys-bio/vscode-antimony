fc67d300541cdcc276f42d8cb7adbc82ced33e1d
evaliu2002 <evaxinerliu@gmail.com>
2023-05-21 20:36:55 -0700

    adding portable python for mac

--- vscode-antimony/src/server/virtualEnvSilicon.sh
+++ vscode-antimony/src/server/virtualEnvSilicon.sh
@@ -22,7 +22,9 @@ ve() {
 
     echo "Creating and activating virtual environment ${venv}"
     # virtualenv ~/[${venv}]
-    sudo -u $USER /Users/evaliu/Downloads/VscodeAntimonySetup/bin/python3.9 -m venv $HOME/${venv} --system-site-package
+    current_script_path=$(dirname "$0")
+    current_path_to_python="$current_script_path/../server/VscodeAntimonySetup/bin/python3.9"
+    sudo -u $USER $current_path_to_python -m venv $HOME/${venv} --system-site-package
     echo $'appdirs==1.4.4\ncertifi==2020.12.5\nchardet==4.0.0\ncolorama==0.4.4\ncolorlog==4.8.0 \neasydev==0.11.0\nidna==2.10\ngit+https://github.com/lark-parser/lark.git@5b8c04ca83b9#egg=lark_parser\nlibChEBIpy==1.0.10\npexpect==4.8.0\nptyprocess==0.7.0\npygls==0.9.1\nrequests==2.25.1\nrequests-cache==0.5.2\nsix==1.15.0\nurllib3==1.26.4\nantimony\nbioservices==1.8.3\n# ols_client==0.0.9\nAMAS-sb==0.0.1\norjson==3.8.0\nnumpy==1.24.2' > $HOME/${venv}/all-requirements.txt
     $HOME/${venv}/bin/python3 -m pip --disable-pip-version-check install -t $HOME/${venv}/lib/python3.9/site-packages --no-cache-dir --upgrade -r $HOME/${venv}/all-requirements.txt && success=1
 }
