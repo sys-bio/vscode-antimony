6c17f4dcb0ea08a18c4abc246b6c7406149dcac0
evaliu2002 <evaxinerliu@gmail.com>
2023-07-11 00:45:29 -0700

    removed commented dependencies

--- vscode-antimony/src/server/virtualEnvLinux.sh
+++ vscode-antimony/src/server/virtualEnvLinux.sh
@@ -8,7 +8,7 @@ ve() {
 
     python3 -m venv $HOME/${venv} --system-site-package
 
-    echo $'appdirs==1.4.4\ncertifi==2020.12.5\nchardet==4.0.0\ncolorama==0.4.4\ncolorlog==4.8.0 \neasydev==0.11.0\nidna==2.10\ngit+https://github.com/lark-parser/lark.git@5b8c04ca83b9#egg=lark_parser\nlibChEBIpy==1.0.10\npexpect==4.8.0\nptyprocess==0.7.0\npygls==0.9.1\nrequests==2.25.1\nrequests-cache==0.5.2\nsix==1.15.0\nurllib3==1.26.4\nantimony\nbioservices==1.8.3\n# ols_client==0.0.9\n# AMAS-sb==0.0.4\norjson==3.8.0' > $HOME/${venv}/all-requirements.txt
+    echo $'appdirs==1.4.4\ncertifi==2020.12.5\nchardet==4.0.0\ncolorama==0.4.4\ncolorlog==4.8.0 \neasydev==0.11.0\nidna==2.10\ngit+https://github.com/lark-parser/lark.git@5b8c04ca83b9#egg=lark_parser\nlibChEBIpy==1.0.10\npexpect==4.8.0\nptyprocess==0.7.0\npygls==0.9.1\nrequests==2.25.1\nrequests-cache==0.5.2\nsix==1.15.0\nurllib3==1.26.4\nantimony\nbioservices==1.8.3\norjson==3.8.0' > $HOME/${venv}/all-requirements.txt
 
     total_steps=$(wc -l < $HOME/${venv}/all-requirements.txt)
     current_step=0
